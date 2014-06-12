972a385a484397ff97d189d6cff0f1f0b9542e52
leemour <leemour@gmail.com>
2014-06-12 09:14:01 +0400

    Convert QuestionsController to Inherited Resources

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,13 @@
 class QuestionsController < InheritedResources::Base
   include ApplicationHelper
 
+  respond_to :html, :js, :json
+
   before_action :authenticate_user!, only: [:favor, :create, :edit, :update, :destroy]
-  before_action :set_question, only: [:favor, :show, :edit, :update, :destroy]
+  # before_action :set_question, only: [:favor, :show, :edit, :update, :destroy]
   before_action :set_user, only: [:favorite, :voted, :by_user]
   before_action :check_permission, only: [:update, :destroy]
-  before_action :set_default_page
+  # before_action :set_default_page
 
   impressionist actions: [:show]
 
@@ -16,9 +18,9 @@ class QuestionsController < InheritedResources::Base
   end
 
   def favor
-    @favored = @question.favor(current_user)
+    @favored = resource.favor(current_user)
     respond_to do |format|
-      format.html { redirect_to @question }
+      format.html { redirect_to resource }
       format.json
     end
   end
@@ -38,70 +40,96 @@ class QuestionsController < InheritedResources::Base
     @questions = Question.where(user_id: params[:user_id]).page(params[:page])
   end
 
-  def index
-    @questions = Question.includes(:tags).page(params[:page])
-    @questions = @questions.send(params[:scope]) if params[:scope]
-  end
+  # def index
+  #   @questions = Question.includes(:tags).page(params[:page])
+  #   @questions = @questions.send(params[:scope]) if params[:scope]
+  # end
 
-  def show
-    @answer = @question.answers.build
-    @comment = @question.comments.build
-  end
+  # def show
+  #   @answer = @question.answers.build
+  #   @comment = @question.comments.build
+  # end
 
-  def new
-    @question = Question.new
-  end
+  # def new
+  #   @question = Question.new
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
   def create
-    @question = current_user.questions.build(question_params)
-    respond_to do |format|
-      if @question.save
-        format.html { redirect_to @question, tr(:question, 'created') }
-        format.js
-      else
-        format.html { render :new }
-        format.js
-      end
-    end
+    create!(tr(:question, 'created'))
   end
+  # def create
+  #   @question = current_user.questions.build(question_params)
+  #   respond_to do |format|
+  #     if @question.save
+  #       format.html { redirect_to @question, tr(:question, 'created') }
+  #       format.js
+  #     else
+  #       format.html { render :new }
+  #       format.js
+  #     end
+  #   end
+  # end
+
 
   def update
-    respond_to do |format|
-      if @question.update(question_params)
-        @comment = @question.comments.build
-        format.html { redirect_to @question, tr(:question, 'updated') }
-        format.js
-      else
-        format.html { render :edit }
-        format.js
-      end
-    end
+    update!(tr(:question, 'updated'))
   end
+  # def update
+  #   respond_to do |format|
+  #     if @question.update(question_params)
+  #       @comment = @question.comments.build
+  #       format.html { redirect_to @question, tr(:question, 'updated') }
+  #       format.js
+  #     else
+  #       format.html { render :edit }
+  #       format.js
+  #     end
+  #   end
+  # end
 
   def destroy
-    @question.destroy
-    respond_to do |format|
-      format.html { redirect_to questions_path, tr(:question, 'deleted') }
-      format.js
-    end
+    destroy!(tr(:question, 'deleted'))
+  end
+  # def destroy
+  #   @question.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to questions_path, tr(:question, 'deleted') }
+  #     format.js
+  #   end
+  # end
+
+  protected
+
+  def create_resource(object)
+    object.user = current_user
+    super
   end
 
-  private
-
-  def set_question
-    @question = Question.includes(answers: [:comments, :attachments, :user]).
-      find(params[:id])
+  def collection
+    @questions ||= end_of_association_chain.page(params[:page])
+    @questions.send(params[:scope]) if params[:scope]
+    @questions
   end
 
-  def set_comment
-    @comment = @question.comments.build
+  def resource
+    @question ||= end_of_association_chain.includes(
+      answers: [:comments, :attachments, :user]).find(params[:id])
   end
+
+  # def set_question
+  #   @question = Question.includes(answers: [:comments, :attachments, :user]).
+  #     find(params[:id])
+  # end
+
+  # def set_comment
+  #   @comment = @question.comments.build
+  # end
 
   def check_permission
-    render_error t('errors.denied') if @question.user != current_user
+    render_error t('errors.denied') if resource.user != current_user
   end
 
   def set_user
@@ -109,7 +137,7 @@ class QuestionsController < InheritedResources::Base
   end
 
   def question_params
-    params[:question][:tag_list] ||= []
+    params[:question][:tag_list] ||= [] if params[:question]
     params.require(:question).permit(:title, :body, tag_list: [],
       attachments_attributes: [:id, :file, :_destroy])
   end
--- app/views/questions/favor.json.jbuilder
+++ app/views/questions/favor.json.jbuilder
@@ -1,3 +1,3 @@
-json.selector class_with_id(@question)
+json.selector class_with_id(resource)
 json.favored  !!@favored
 json.text @favored ? t('favorite.favored') : t('favorite.favor')
